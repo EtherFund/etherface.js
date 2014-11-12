671e5793c3bf81209b51a774c1c39020e7adc4bf
J.R. Bédard <jrbedard@gmail.com>
2014-11-12 13:26:16 -0800

    dev

--- etherface.js
+++ etherface.js
@@ -9,10 +9,13 @@ function Etherface(args) {
 	this.version = '0.1';
 	this.key = args.key;
 	this.app = args.app;
-	this.url = "http://api.ether.fund:80";
-	this.hostname = document.location.hostname;
-	if(this.hostname == "localhost") {
-		this.url = "http://localhost:3000"; // local debug
+	var hostname = document.location.hostname;
+	if(hostname == "localhost") {
+		this.hostname = "http://localhost:3000"; // local debug
+		this.port = 3000;
+	} else {
+		this.hostname = "http://api.ether.fund:80";
+		this.port = 80;
 	}
 	this.socket = null;
 	this.type = null;
@@ -22,13 +25,13 @@ function Etherface(args) {
 
 // SOCKET
 Etherface.prototype.connect = function(data, con, dis, confail) {
-	var that=this;
-	this.socket = io(this.url, {key: this.key, app: this.app});//, transports:['websocket']});
+	var self=this;
+	this.socket = io(this.hostname, {key: this.key, app: this.app});//, transports:['websocket']});
 	// ["xhr-polling", "flashsocket", "htmlfile", "jsonp-polling", "websocket"]
 	
 	this.socket.on('connect', function() {
-		if(that.socket.io && that.socket.io.engine && that.socket.io.engine.transport ) {
-			that.type = that.socket.io.engine.transport.name;
+		if(self.socket.io && self.socket.io.engine && self.socket.io.engine.transport ) {
+			self.type = self.socket.io.engine.transport.name;
 		}
 		if(con) { con(); }
 		
@@ -45,7 +48,6 @@ Etherface.prototype.connect = function(data, con, dis, confail) {
 
 // SOCKET SEND
 Etherface.prototype.send = function(cmd, args, done) {
-	var that=this;
 	if(!this.socket) { return; }
 	this.socket.emit('etherface', {cmd:cmd, args:args}, function(data) {
 		if(done) { done(data); }
@@ -54,148 +56,158 @@ Etherface.prototype.send = function(cmd, args, done) {
 
 
 
-//  R E S T
+//  R E S T endpoints
 
 // STATUS
-Etherface.prototype.status = {
-	get: function(fn) {
-		this.get('/status', fn);
-	},
-	schema: function(fn) {
-		this.get('/schema', fn);
-	},
+Etherface.prototype.status = function(cmd,args,fn) {
+	if(cmd=='get') {
+		this.get('/status',args,fn);
+	}
+	else if(cmd=='schema') {
+		this.get('/schema',args,fn);
+	}
+	else {
+		return fn('invalid status method');
+	}
 };
 
 
 // CONTRACT
-Etherface.prototype.contract = {
-	
-	_checkId : function() {
-	},
-	
-	list : function(args,fn) {
-		this.get('/contracts', fn);
-	},
-	create : function(args,fn) {
-		this.put('/contracts', fn);
-	},
-	delete : function(id,fn) {
-		this.delete('/contracts', fn);
-	},
-	
-	view : function(id,fn) {
-		this.get('/contracts/'+id, fn);
-	},
-	update : function(id,fn) {
-		this.post('/contracts/'+id, fn);
-	},
-	validate : function(id,fn) {
-		this.get('/contracts/'+id+'/validate', fn);
-	},
-	compile : function(id,fn) {
-		this.post('/contracts/'+id+'/compile', fn);
-	},
-	simulate : function(id,fn) {
-		this.post('/contracts/'+id+'/simulate', fn);
-	},
-	audit : function(id,fn) {
-		this.post('/contracts/'+id+'/audit', fn);
+Etherface.prototype.contract = function(cmd,args,fn) {
+	if(cmd=='list') {
+		//default args
+		this.get('/contracts',args,fn);
+	}
+	else if(cmd=='create') {
+		this.put('/contracts',args,fn);
+	}
+	else if(cmd=='view') {
+		this.get('/contracts/'+args.id,args,fn);
+	}
+	else if(cmd=='update') {
+		this.post('/contracts/'+args.id,args,fn);
+	}
+	else if(cmd=='delete') {
+		this.delete('/contracts/'+args.id,args,fn);
+	}
+	else if(cmd=='validate') {
+		this.get('/contracts/'+args.id+'/validate',args,fn);
+	}
+	else if(cmd=='compile') {
+		this.post('/contracts/'+args.id+'/compile',args,fn);
+	}
+	else if(cmd=='simulate') {
+		this.post('/contracts/'+args.id+'/simulate',args,fn);
+	}
+	else if(cmd=='audit') {
+		this.post('/contracts/'+args.id+'/audit',args,fn);
+	}
+	else {
+		return fn('invalid contract method');
 	}
 };
 
 
 // TRANSACTION
-Etherface.prototype.transaction = {
+Etherface.prototype.transaction = function(cmd,args,fn) {
 };
 
 // ACCOUNT
-Etherface.prototype.account = {
+Etherface.prototype.account = function(cmd,args,fn) {
 };
 
 // BLOCK
-Etherface.prototype.block = {
+Etherface.prototype.block = function(cmd,args,fn) {
 };
 
 // ANALYTICS
-Etherface.prototype.analytics = {
+Etherface.prototype.analytics = function(cmd,args,fn) {
 };
 
 
 // ETHER
-Etherface.prototype.ether = {
-	get: function(fn) {
-		this.get('/ether', fn);
-	},
-	gas: function(fn) {
-		this.get('/ether/gas', fn);
-	},
-	faucet: function(fn) {
-		this.post('/ether/faucet', fn);
-	},
+Etherface.prototype.ether = function(cmd,args,fn) {
+	if(cmd=='get') {
+		this.get('/ether',args,fn);
+	}
+	else if(cmd=='gas') {
+		this.get('/ether/gas',args,fn);
+	}
+	else if(cmd=='faucet') {
+		this.post('/ether/faucet',args,fn);
+	}
+	else {
+		return fn('invalid ether method');
+	}
 };
 
 
 // BITCOIN
-Etherface.prototype.bitcoin = {
-	get: function(fn) {
-		this.get('/bitcoin', fn);
-	},
+Etherface.prototype.bitcoin = function(cmd,args,fn) {
+	if(cmd=='get') {
+		this.self.get('/bitcoin',args,fn);
+	}
+	else {
+		return fn('invalid bitcoin method');
+	}
 };
 
 
 // CURRENCY
-Etherface.prototype.currency = {
-	list: function(fn) {
-		this.get('/currencies', fn);
-	},
-	get: function(sym,fn) {
-		this.get('/currencies/'+sym, fn);
-	},
+Etherface.prototype.currency = function(cmd,args,fn) {
+	if(cmd=='list') {
+		this.get('/currencies',args,fn);
+	}
+	else if(cmd=='get') {
+		this.get('/currencies/'+args.sym,args,fn);
+	}
+	else {
+		return fn('invalid currency method');
+	}
 };
 
 
 
 // GET
-Etherface.prototype.get = function(url, done) {
-	$.get(this.url+url, function(data) {
-		done(data);
-	});
+Etherface.prototype.get = function(path, data, done, error) {
+	return this.ajax('GET',path,data,done,error);
 };
 
 // POST
-Etherface.prototype.post = function(url, data, done, error) {
-	// Ajax POST
+Etherface.prototype.post = function(path, data, done, error) {
+	return this.ajax('POST',path,data,done,error);
+};
+
+// PUT
+Etherface.prototype.put = function(path, data, done, error) {
+	return this.ajax('PUT',path,data,done,error);
+};
+
+// DELETE
+Etherface.prototype.delete = function(path, data, done, error) {
+	return this.ajax('DELETE',path,data,done,error);
+};
+
+// AJAX
+Etherface.prototype.ajax = function(type, path, data, done, error) {
 	data.csrfmiddlewaretoken = gCsrfToken;
-	$.ajax({type:'POST', cache:false, url: url, data: data,
+	var url = this.hostname + path;
+	$.ajax({type:type, cache:false, url:url, data:data,
 		beforeSend:function(xhr, settings) {
 		},
 	  	success:function(data) {
 			if('error' in data) {
+				console.log("Etherface backend error on "+type+" "+url+" : "+data);
 				error(data);
 			} else {
 				done(data);
 			}
 	  	},
 	  	error:function() {
+			console.log("Etherface error on "+type+" "+url+" : "+data);
 			error(data);
 	  	}
 	});
 };
-
-// PUT
-Etherface.prototype.put = function(url, data, done, error) {
-	$.ajax({type:'PUT', cache:false, url: url, data: data});
-};
-
-// DELETE
-Etherface.prototype.delete = function(url, data, done, error) {
-	$.ajax({type:'DELETE', cache:false, url: url, data: data});
-};
-
-
-
-
-
-
 
 
